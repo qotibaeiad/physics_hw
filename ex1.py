32f3f4292a91a0e3db2c87d7f2d971aae2ed6c33
user <user@users-MacBook-Pro.local>
2024-01-14 12:10:28 +0200

    first commit

--- ex1.py
+++ ex1.py
@@ -1,18 +1,15 @@
+import matplotlib.pyplot as plt
 import numpy as np
-#input
-A=float(input("A:"))
-B=float(input("B:"))
-qn=int(input("qn:"))
 
-# you code
-##########
+#input q#
+qn=int(input())
 
+# your code
+A="maagal"  #example
+B=""
 
-##########
 #output
-if qn==1: 
-    print(f'{C:.5g}')
-elif qn==2: 
-    print(f'{D:.5g}')
-elif qn==3:
-    print(max_no)
+if qn==1:
+    print(A)
+elif qn==2:
+    print(B)
