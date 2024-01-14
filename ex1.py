ab1cc6c6c5923cbef99a96e3f72c204d2ce5a794
user <user@users-MacBook-Pro.local>
2024-01-14 09:02:17 +0200

    first commit

---
+++ ex1.py
@@ -0,0 +1,18 @@
+import numpy as np
+#input
+A=float(input("A:"))
+B=float(input("B:"))
+qn=int(input("qn:"))
+
+# you code
+##########
+
+
+##########
+#output
+if qn==1: 
+    print(f'{C:.5g}')
+elif qn==2: 
+    print(f'{D:.5g}')
+elif qn==3:
+    print(max_no)
