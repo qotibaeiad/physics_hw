806e322101d8883c64c29b6e854e50a202daf175
user <user@users-MacBook-Pro.local>
2024-01-20 23:33:45 +0200

    pull request

--- Draw.py
+++ Draw.py
@@ -1,42 +1,45 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-#input q#
-qn=int(input())
+# input q
+qn = int(input())
 
 # your code
-A="maagal"  #example
-B="heberbola"
+A = "maagal"  # example
+B = "heberbola"
 
-if qn==1:
-    theta = np.linspace(0, 2*np.pi, 100)
+if qn == 1:
+    theta = np.linspace(0, 2 * np.pi, 1000)
     x = np.cos(theta)
     y = np.sin(theta)
+    
     plt.plot(x, y, label=r'$x^2 + y^2 = 1$')
     plt.title('Circle: $x^2 + y^2 = 1$')
     plt.xlabel('x')
     plt.ylabel('y')
-if qn==2:
-    x = np.linspace(-10,10,100)
-    y1 = np.sqrt(1-x**2)+x**(3/2)
-    y2 = -np.sqrt(1-x**2)+x**(3/2)
+
+elif qn == 2:
+    x = np.linspace(-10, 10, 1000)
+    y1 = np.sqrt(1 - x**2) + x**(3/2)
+    y2 = -np.sqrt(1 - x**2) + x**(3/2)
+    
     plt.plot(x, y1, label=r'$y = \sqrt{1 - x^2} + \frac{x^2}{3}$')
     plt.plot(x, y2, label=r'$y = -\sqrt{1 - x^2} + \frac{x^2}{3}$')
-   # plt.title('Curve: $y = \sqrt{1 - x^2} + \frac{x^2}{3}$')
+    # plt.title('Curve: $y = \sqrt{1 - x^2} + \frac{x^2}{3}$')
     plt.xlabel('x')
     plt.ylabel('y')
 
 plt.grid(True)
-plt.axhline(0, color='black',linewidth=0.5)
-plt.axvline(0, color='black',linewidth=0.5)
-plt.gca().set_aspect('equal', adjustable='box') # Equal aspect ratio
+plt.axhline(0, color='black', linewidth=0.5)
+plt.axvline(0, color='black', linewidth=0.5)
+plt.gca().set_aspect('equal', adjustable='box')  # Equal aspect ratio
+
 # Display the plot
 plt.legend()
 plt.show()
 
-
-#output
-if qn==1:
+# output
+if qn == 1:
     print(A)
-elif qn==2:
+elif qn == 2:
     print(B)
---
+++ s.py
@@ -0,0 +1,17 @@
+import numpy as np
+
+Ax=float(input("Ax: "))
+Ay=float(input("Ay: "))
+Az=float(input("Az: "))
+B=np.array([3,4,-2])
+## your code:
+sum=0
+A=np.array([Ax,Ay,Az])
+for i in range (0,3):
+    sum+=B[i]*A[i]
+len1=np.sqrt(9+16+4)
+len2=np.sqrt((Ax**2)+(Ay**2)+(Az**2))
+alpha = np.arccos(sum/(len1*len2))
+alpha = np.degrees(alpha)
+##
+print(alpha)
