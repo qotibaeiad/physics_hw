45709da47fbddd8e1d5717913261d9e0c70c01d4
qotibamhamed <qotibaeiad98@gmail.com>
2024-01-27 11:51:43 +0200

    almost finish

--- week2/ex1.py
+++ week2/ex1.py
@@ -2,36 +2,31 @@ import numpy as np
 
 qn = int(input())
 
+
+
+A=np.arange(0.0, 10.0, 1.0)
+B=np.power(10.0, np.arange(10.0))-1.0
+C=np.diff(B)
+D=np.flip(C)
+E=np.cumsum(D)
+x=np.arange(0.0, 6.28, 0.01)
+f=np.power(np.sin(x),2.0)
+I=np.sum(np.power(np.sin(x),2.0) * 0.01)
 if qn == 1:
-    result = np.arange(0.0, 10.0, 1.0)
-    print([round(num, 5) for num in result])
+    print(A)
 elif qn == 2:
-    result = np.power(10.0, np.arange(10.0))-1.0
-    print(result)
+    print(B)
 elif qn == 3:
-    b = np.power(10.0, np.arange(10.0))-1.0
-    c = np.diff(b)
-    print(c)
+    print(C)
 elif qn==4:
-    b = np.power(10.0, np.arange(10.0))-1.0
-    c = np.diff(b)
-    d=np.flip(c)
-    print(d)
+    print(D)
 elif qn==5:
-    b = np.power(10.0, np.arange(10.0))-1.0
-    c = np.diff(b)
-    d=np.flip(c)
-    e=np.cumsum(d)
-    print(e)
+    print(E)
 elif qn==6:
-    x=np.arange(0.0,6.28,0.01)
-    y = np.power(np.sin(x),2.0)
-    print(y[:10])
+    print(f)
 elif qn==7:
-    a, b, d = 0, 6.28, 0.01
-    x = np.arange(a, b, d)
-    sum = np.sum(np.power(np.sin(x),2.0) * d)
-    print(sum)
+    print(I)
 elif qn==8:
-    x=np.arange(0.0,6.28,0.01)
-    y = np.power(np.sin(x),2.0)
+    x = np.arange(0.0, 6.28, 0.01)
+    y = np.power(np.sin(x), 2.0)
+    
