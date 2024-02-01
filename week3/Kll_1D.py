0dd2bce6de48d77844c37ee02d6423a164a6391d
user <user@users-MacBook-Pro.local>
2024-02-01 13:44:41 +0200

    2 week 3

--- week3/Kll_1D.py
+++ week3/Kll_1D.py
@@ -6,9 +6,15 @@ qn=int(input('enter question num: '))
 
 
 t=np.arange(0,7,0.01)
-v = v0*np.exp(t*(-gama))*np.cos(10*gama*t)
+v=v0*np.exp(t*(-gama))*np.cos(10*gama*t)
+a=np.diff(v)/np.diff(t)
+ones=np.ones(len(t))
+
+vx = np.ones(len(t)) * v0
 
 if qn==1:
     print(t[:10])
 if qn==2:
     print(v[:10])
+if qn==3:
+    print(a[:10])
